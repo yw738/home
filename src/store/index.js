ac46f37f89450aac8089f98de86bcaa8c6881ad3
yangwei <738751286@qq.com>
2023-02-03 16:17:52 +0800

    1

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 // 创建一个新的 store 实例
 export default createStore({
     state() {
         return {
             isPrivate: false, //是否为私密模式 ，开启隐藏菜单
-        }
+        };
     },
     mutations: {
         /**
@@ -12,7 +12,7 @@ export default createStore({
          * @param {boolean} boolean 传入的布尔值
          */
         setPrivate(state, boolean) {
-            state.isPrivate = boolean
-        }
-    }
-})
+            state.isPrivate = boolean;
+        },
+    },
+});
