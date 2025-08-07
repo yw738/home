3b420be1eaa77623f0e5bff47b04214ddc19f30b
yw738 <https://github.com/yw738>
2025-08-07 17:43:21 +0800

    修改打包配置 解决404的问题

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ import plugin from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import { VantResolver } from "@vant/auto-import-resolver";
 import legacy from "@vitejs/plugin-legacy";
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +19,23 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         api: "modern-compiler", // or 'modern'
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : "";
+          // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+          // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
       },
     },
   },
